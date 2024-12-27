54f00f522aae454a97f654c1fe90446208f78698
Shougata Das <shougatad@gmail.com>
2024-12-27 21:20:12 +0600

    Changes Made

--- greedy_best_first_search.py
+++ greedy_best_first_search.py
@@ -1,5 +1,6 @@
 import heapq
 
+path = []
 def greedy_best_first_search(graph, start, goal, heuristics):
     visited = set()
     priority_queue = []
@@ -9,7 +10,7 @@ def greedy_best_first_search(graph, start, goal, heuristics):
         _, current = heapq.heappop(priority_queue)
         if current in visited:
             continue
-        print(f"Visited: {current}")
+        path.append(current)
         visited.add(current)
 
         if current == goal:
@@ -24,15 +25,53 @@ def greedy_best_first_search(graph, start, goal, heuristics):
     return False
 
 graph = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F'],
-    'D': [],
-    'E': ['F'],
-    'F': []
+    'Arad': {'Zerind':75, 'Sibiu':140, 'Timisoara':118},
+    'Zerind': {'Arad':75, 'Oradea':71},
+    'Oradea': {'Zerind':71, 'Sibiu':151},
+    'Sibiu': {'Arad':140, 'Oradea':151, 'Fagaras':99, 'Rimnicu Vilcea':80},
+    'Fagaras': {'Sibiu':99, 'Bucharest':211},
+    'Rimnicu Vilcea': {'Sibiu':80, 'Pitesti':97, 'Craiova':146},
+    'Pitesti': {'Rimnicu Vilcea':97, 'Craiova':138, 'Bucharest':101},
+    'Timisoara': {'Arad':118, 'Lugoj':111},
+    'Lugoj': {'Timisoara':111, 'Mehadia':70},
+    'Mehadia': {'Lugoj':70, 'Drobeta':75},
+    'Drobeta': {'Mehadia':75, 'Craiova':120},
+    'Craiova': {'Drobeta':120, 'Rimnicu Vilcea':146, 'Pitesti':138},
+    'Bucharest': {'Fagaras':211, 'Pitesti':101, 'Giurgiu':90, 'Urziceni':85},
+    'Giurgiu': {'Bucharest':90},
+    'Urziceni': {'Bucharest':85, 'Hirsova':98, 'Vaslui':142},
+    'Hirsova': {'Urziceni':98, 'Eforie':86},
+    'Eforie': {'Hirsova':86},
+    'Vaslui': {'Urziceni':142, 'Iasi':92},
+    'Iasi': {'Vaslui':92, 'Neamt':87},
+    'Neamt': {'Iasi':87}
 }
-heuristics = {'A': 10, 'B': 8, 'C': 5, 'D': 7, 'E': 3, 'F': 0}
-start = 'A'
-goal = 'F'
+heuristics = {
+    'Arad': 366,
+    'Bucharest': 0,
+    'Craiova': 160,
+    'Drobeta': 242,
+    'Eforie': 161,
+    'Fagaras': 176,
+    'Giurgiu': 77,
+    'Hirsova': 151,
+    'Iasi': 226,
+    'Lugoj': 244,
+    'Mehadia': 241,
+    'Neamt': 234,
+    'Oradea': 380,
+    'Pitesti': 100,
+    'Rimnicu Vilcea': 193,
+    'Sibiu': 253,
+    'Timisoara': 329,
+    'Urziceni': 80,
+    'Vaslui': 199,
+    'Zerind': 374
+}
+
+start = 'Arad'
+goal = 'Bucharest'
 
 greedy_best_first_search(graph, start, goal, heuristics)
+
+print(f"Path: {path}")
